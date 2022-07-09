dc6ced40d2222588674c94080747212e54579764
tkeru98 <JennerTK@gmail.com>
2022-07-08 22:42:12 -0600

    init commit lexical analyzer

---
+++ scanType.h
@@ -0,0 +1,15 @@
+#ifndef _SCANTYPE_H_
+#define _SCANTYPE_H_
+// 
+//  SCANNER TOKENDATA
+// 
+struct TokenData {
+    int  tokenclass;        // token class
+    int  linenum;           // line where found
+    char *tokenstr;         // what string was actually read
+    char cvalue;            // any character value
+    int  nvalue;            // any numeric value or Boolean value
+    char *svalue;           // any string value e.g. an id
+    int slen;               // string length of svalue
+};
+#endif
